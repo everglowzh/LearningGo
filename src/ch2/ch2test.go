022a08b2acc70c60764bd025825be040353f44bd
everglowzh <everglowzh@qq.com>
2020-02-11 22:40:34 +0800

    test

--- src/ch2/ch2test.go
+++ src/ch2/ch2test.go
@@ -1,16 +1,6 @@
 package main
 
 import "fmt"
+medals := []string{"gold", "silver", "bronze"}
 
-var global *int
 
-func main() {
-	f()
-	fmt.Println(*global)
-}
-
-func f() {
-	var x int
-	x = 1
-	global = &x
-}
